d80e0997f98f6d3dee7be642dee31b551a8aa113
Joshua <joshuaystang@gmail.com>
2017-08-31 17:22:44 +1000

    grouped widgets toggle handle into one function

--- server.R
+++ server.R
@@ -1,6 +1,18 @@
 library(shiny)
 library(shinyjs)
 
+# Given with a list of widgets, enable or disable all the widgets depending on the given bool
+toggle_widgets <- function(widgets, is_enable) {
+    for(widget in widgets) {
+        if (is_enable) {
+            enable(widget)
+        } else {
+            disable(widget)
+        }
+    }
+}
+
+# Toggles heterozygosity widget
 toggle_heterozygosity <- function(input) {
     if(input$sim_genome_type == "sim_diploid") {
         enable("sim_heterozygosity")
@@ -10,35 +22,23 @@ toggle_heterozygosity <- function(input) {
 }
 
 shinyServer(function(input, output, session) {
-    # disable simulation by default
-    disable("sim_genome_size")
-    disable("sim_genome_type")
-    disable("sim_heterozygosity")
+    input_widgets = c("kmer_file", "sample", "kmer_length", "read_length", "max_kmer_coverage")
+    sim_widgets = c("sim_genome_size", "sim_genome_type", "sim_heterozygosity")
     
-    # if switching to simulation swap focus and disable file input
+    # disable simulation by default
+    toggle_widgets(sim_widgets, FALSE)
+    
+    # if switching to simulation swap focus, disable input settings and enable simulation settings
     observeEvent(input$simulation, {
-        disable("kmer_file")
-        disable("sample")
-        disable("kmer_length")
-        disable("read_length")
-        disable("max_kmer_coverage")
-        
-        enable("sim_genome_size")
-        enable("sim_genome_type")
+        toggle_widgets(input_widgets, FALSE)
+        toggle_widgets(sim_widgets, TRUE)
         toggle_heterozygosity(input)
     })
     
-    # if switching to user input switch focus and disable simulation
+    # if switching to user input switch focus, disable simulation and enable input settings
     observeEvent(input$user_input, {
-        disable("sim_genome_size")
-        disable("sim_genome_type")
-        disable("sim_heterozygosity")
-        
-        enable("kmer_file")
-        enable("sample")
-        enable("kmer_length")
-        enable("read_length")
-        enable("max_kmer_coverage")
+        toggle_widgets(sim_widgets, FALSE)
+        toggle_widgets(input_widgets, TRUE)
     })
     
     # listener to enable heterozygosity only for diploid genomes
