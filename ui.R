f8397e6d0cf2db8f1b7500605676c0f301233f1e
Joshua <joshuaystang@gmail.com>
2017-10-25 16:30:19 +1100

    tidied up

--- ui.R
+++ ui.R
@@ -13,13 +13,8 @@ shinyUI(
         navbarPage(title="GenomeR", id="navbar",
             tabPanel("Model", value="nav_models", mainPage()),
             tabPanel("Results", value="nav_results", resultsPage()),
-# <<<<<<< HEAD
-#             tabPanel("Batch Analysis", value = "nav_batch", batchPage()),
-#             tabPanel("Help", includeMarkdown("Readme.md"))
-# =======
             tabPanel("Batch Analysis", value="nav_batch", batchPage()),
             tabPanel(title=HTML("</a></li><li><a href='https://bitbucket.org/violetbrina/binf3111-genomer/wiki/Home' target='_blank'>Help</a></li>"), value="nav_help")
-# >>>>>>> origin/sabrina
         )
     )
 )
