4fe188adffd1c3b6c1c2f3e63e82c1c4646d8005
ziyingzhang1 <39799544+ziyingzhang1@users.noreply.github.com>
2018-11-16 11:27:06 +1100

    Delete requirements.R
    User do not need to run this command anymore(Rscript requirements.R).
    Set up packages when they start the GenomeR.

--- requirements.R
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env Rscript
-
-dependencies <- c("shiny", "shinyjs", "shinyWidgets", "shinycssloaders", "plotly", "quantmod", "tidyverse")
-install.packages(dependencies, repos = "https://cloud.r-project.org/")
