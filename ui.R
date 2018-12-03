b9e8a21da6d6511be787b9ec8c94f14f892aedd1
ziyingzhang1 <39799544+ziyingzhang1@users.noreply.github.com>
2018-12-03 12:35:28 +1100

    Create a new page to give  an introduction to GenomeR

--- ui.R
+++ ui.R
@@ -6,11 +6,13 @@ library(plotly)
 source("mainPage.R")
 source("resultsPage.R")
 source("batchPage.R")
+source("Instruction.R")
 
 shinyUI(
     tagList(
         useShinyjs(),
         navbarPage(title="GenomeR", id="navbar",
+            tabPanel("Instruction",Instruction()),
             tabPanel("Model", value="nav_models", mainPage()),
             tabPanel("Results", value="nav_results", resultsPage()),
             tabPanel("Batch Analysis", value="nav_batch", batchPage()),
